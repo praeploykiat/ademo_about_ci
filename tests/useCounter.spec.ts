0a9acf039b3842ad1213a90d63af1cf2dc278e97
Praeploy Kiatsuksri <praeploy05@gmail.com>
2025-08-07 10:41:46 +0700

    feat: init useCounter.spec.ts

---
+++ tests/useCounter.spec.ts
@@ -0,0 +1,35 @@
+import { renderHook, act } from "@testing-library/react";
+import useCounter from "../src/hooks/features/homepage/useCounter";
+import { describe, it, expect } from "vitest";
+
+describe("useCounter", () => {
+  it("should initialize count to 0 and val to 1", () => {
+    const { result } = renderHook(() => useCounter());
+    expect(result.current.count).toBe(0);
+    expect(result.current.val).toBe(1);
+  });
+
+  it("should increment count by val when increment is called", () => {
+    const { result } = renderHook(() => useCounter());
+
+    act(() => {
+      result.current.increment();
+    });
+    expect(result.current.count).toBe(1);
+
+    act(() => {
+      result.current.setVal(5);
+      result.current.increment();
+    });
+    expect(result.current.count).toBe(6);
+  });
+
+  it("should allow setting a new value for val", () => {
+    const { result } = renderHook(() => useCounter());
+
+    act(() => {
+      result.current.setVal(3);
+    });
+    expect(result.current.val).toBe(3);
+  });
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
-import { defineConfig } from 'vite';
-import path from 'path';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from "vitest/config";
+import path from "path";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@/': `${path.resolve(__dirname, './src')}/`,
+      "@/": `${path.resolve(__dirname, "./src")}/`,
     },
+  },
+  test: {
+    environment: "jsdom",
+    globals: true,
   },
 });
